98d7032d26d9775124cf3c17e19c0b3117884bd2
cleap <cleapdev@gmail.com>
2019-02-24 16:08:49 -0700

    Added checks to determine if an expression is evaluatable

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,46 @@
+struct Operation {
+    name: char,
+}
+
+fn op(name: char) -> Operation {
+    Operation {
+        name,
+    }
+}
+
 struct Expression {
     val: String,
     terms: Vec<Expression>,
     op: char,
 }
 
-fn expr(val: String) -> Expression {
-    let operations = ['=', '+', '-', '*', '/', '^'];
+fn expr(str_rep: String, operations: &Vec<Operation>) -> Expression {
 
-    let mut ex = Expression {
-        val,
-        terms: Vec::new(),
-        op: '.',
-    };
+    let val = String::from(str_rep);
+    let mut terms: Vec<Expression> = Vec::new();
+    let mut op = '.';
 
-    for operation in &operations {
-        let subexes: Vec<&str> = ex.val.split(*operation).collect();
+    for operation in operations {
+        let subexes: Vec<&str> = val.split(operation.name).collect();
         if subexes.len() > 1 {
-            ex.op = *operation;
+            op = operation.name;
             for subex in subexes {
-                ex.terms.push(expr(String::from(subex)));
+                terms.push(expr(String::from(subex), operations));
             }
             break;
         }
     }
-    ex
+
+    Expression {
+        val,
+        terms,
+        op,
+    }
 }
 
 fn print_ex(ex: &Expression) {
     if ex.terms.len() == 0 {
-        print!("{} ", ex.val);
+        print!("{}", ex.val);
     } else {
         print!("( ");
         let mut i = ex.terms.len();
@@ -39,13 +51,39 @@ fn print_ex(ex: &Expression) {
             }
             i -= 1;
         }
-        print!(") ");
+        print!(")");
+    }
+    if is_evalable(&ex) {
+        print!("_e ");
+    } else {
+        print!(" ");
+    }
+}
+
+fn is_evalable(ex: &Expression) -> bool {
+    if ex.terms.len() == 0 {
+        !ex.val.parse::<f32>().is_err()
+    } else {
+        let mut res = true;
+        for term in &ex.terms {
+            if !is_evalable(term) {
+                res = false
+            }
+        }
+        res
     }
 }
 
 fn main() {
-    println!("Hello, world!");
-    let ex = expr(String::from("y*5+z^2=a*x^2+b*x+c"));
-    print_ex(&ex);
+    let mut operations: Vec<Operation> = Vec::new();
+    operations.push(op('='));
+    operations.push(op('-'));
+    operations.push(op('+'));
+    operations.push(op('/'));
+    operations.push(op('*'));
+    operations.push(op('^'));
 
+    let ex = expr(String::from("y*5+4/2=a*x^2+b*x+c+5*3"), &operations);
+    print_ex(&ex);
+    println!();
 }
